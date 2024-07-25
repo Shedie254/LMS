2543bdc6b21be7656fa70090fda23e1121942203
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-07-25 17:46:23 +0300

    new database schema

--- LibraryManagementSystem/lib/database.sql
+++ LibraryManagementSystem/lib/database.sql
@@ -1,42 +1,153 @@
-CREATE DATABASE library_db;
-USE library_db;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 25, 2024 at 02:43 PM
+-- Server version: 10.11.6-MariaDB-0+deb12u1
+-- PHP Version: 8.2.20
 
-CREATE TABLE IF NOT EXISTS `Roles` (
-    `role_id` ENUM('admin', 'librarian', 'user') NOT NULL,
-    PRIMARY KEY (`role_id`)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(50) UNIQUE NOT NULL,
-    `email` VARCHAR(50) UNIQUE NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `role_id` ENUM('admin', 'librarian', 'user') NOT NULL,
-    CONSTRAINT `fk_user_role` FOREIGN KEY (`role_id`) REFERENCES `Roles`(`role_id`)
-);
 
-CREATE TABLE IF NOT EXISTS `Books` (
-    `book_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `title` VARCHAR(100) NOT NULL,
-    `author` VARCHAR(100) NOT NULL,
-    `genre` VARCHAR(100) NOT NULL,
-    `status` BOOLEAN NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE IF NOT EXISTS `Borrowed_books` (
-    `book_id` INT NOT NULL,
-    `user_id` INT NOT NULL,
-    `issue_date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    `return_date` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    CONSTRAINT `fk_book_id` FOREIGN KEY (`book_id`) REFERENCES `Books`(`book_id`),
-    CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
-    PRIMARY KEY (`book_id`, `user_id`)
-);
+--
+-- Database: `library_db`
+--
 
-CREATE TABLE IF NOT EXISTS `Fines` (
-    `fine_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT NOT NULL,
-    `fine_amount` DECIMAL(10, 2) NOT NULL,
-    `paid` BOOLEAN NOT NULL,
-    CONSTRAINT `fk_user_id_fines` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
-);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `genre` varchar(100) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `coverpage` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowed_books`
+--
+
+CREATE TABLE `borrowed_books` (
+  `book_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `issue_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `return_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fines`
+--
+
+CREATE TABLE `fines` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fine_amount` decimal(10,2) NOT NULL,
+  `paid` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','librarian','user') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `borrowed_books`
+--
+ALTER TABLE `borrowed_books`
+  ADD PRIMARY KEY (`book_id`,`user_id`),
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- Indexes for table `fines`
+--
+ALTER TABLE `fines`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_id_fines` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fines`
+--
+ALTER TABLE `fines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrowed_books`
+--
+ALTER TABLE `borrowed_books`
+  ADD CONSTRAINT `fk_book_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `fines`
+--
+ALTER TABLE `fines`
+  ADD CONSTRAINT `fk_user_id_fines` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
